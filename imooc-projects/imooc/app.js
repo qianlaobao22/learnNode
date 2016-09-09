c277a5867eca7f2aea5d3aa90486b9a1fa64d4ee
qianlaobao22 <511718974@qq.com>
2016-09-09 17:24:12 +0800

    加入评论功能，没有回复的版本

--- imooc-projects/imooc/app.js
+++ imooc-projects/imooc/app.js
@@ -4,15 +4,13 @@ var path = require('path')
 var mongoose = require('mongoose')
 var session = require('express-session')
 var mongoStore = require('connect-mongo')(session)
-var _ = require('underscore')
-var Movie = require('./models/movie')
-var User = require('./models/user')
+
 var port = process.env.PORT || 3000
 var app = express()
 var dbUrl = 'mongodb://127.0.0.1/imooc'
 var cookieParser = require('cookie-parser')
+var logger = require('morgan')
 
-//我做了点修改，看看管不管用
 
 mongoose.Promise = global.Promise
 mongoose.connect(dbUrl)
@@ -27,7 +25,15 @@ app.use(session({
 
     })
 }))
-app.set('views','./views/other')
+
+if('delelopment'===app.get('env')){
+    app.set('showStackError') = true
+    app.use(logger(':method :url :status'))
+    app.locals.pretty = true
+    mongoose.set('debug',true)
+}
+
+app.set('views','./app/views/other')
 app.set('view engine','jade')
 //app.use(express.bodyParser())
 //app.use(bodyParser)
@@ -44,261 +50,5 @@ app.listen(port)
 
 console.log('imoo started on port '+port)
 
-//pre handle user
 
-app.use(function(req,res,next){
-     var _user = req.session.user
-
-     if(_user){
-       
-        app.locals.user = _user
-     }
-
-     return next()
-    
-})
-
-
-
-//index page 
-
-
-app.get('/',function(req,res){
-    console.log('user in session:')
-    console.log(req.session.user)
-
-    
-
-	Movie.fetch(function(err,movies){
-      if(err){
-      	console.log(err)
-      }
-
-     
-     res.render('index',{
-		title:'imooc 首页',
-		movies:movies
-	}) 
-  })
-})
-
-
-//signup
-
-app.post('/user/signup', function(req,res){
-    var _user = req.body.user
-    
-    User.find({name:_user.name},function(err,user){
-        if(err){
-            console.log(err)
-        }
-
-        if(user){
-            return res.redirect('/')
-        }
-        else{
-
-
-          var user = new User(_user)
-  
-           user.save(function(err,user){
-        if(err){
-            console.log(err)
-        }
-        res.redirect('/admin/userlist')
-    }) 
-        }
-    })
-
-
-    
-})
-
-//signin
-app.post('/user/signin',function(req,res){
-    var _user = req.body.user
-    var name = _user.name
-    var password = _user.password
-
-    User.findOne({name:name},function(err,user){
-        if(err){
-            console.log(err)
-        }
-        if(!user){
-            return res.redirect('/')
-        }
-
-        user.comparePassword(password,function(err,isMatch){
-            if(err){
-                console.log(err)
-            }
-            if(isMatch){
-                req.session.user =user
-                return res.redirect('/')
-            }
-            else{
-                console.log('password is not matched')
-            }
-        })
-    })
-})
-
-//logout
-
-app.get('/logout',function(req,res){
-    delete req.session.user
-    delete app.locals.user
-    res.redirect('/')
-
-})
-
-
-//userlist page 
-
-
-app.get('/admin/userlist',function(req,res){
-    User.fetch(function(err,users){
-      if(err){
-        console.log(err)
-      }
-
-     
-     res.render('userlist',{
-        title:'imooc 用户列表页',
-        users:users
-    })
-  })
-})
-
-
-
-
-//detail page 
-
-
-app.get('/movie/:id',function(req,res){
-	var id = req.params.id
-
-	Movie.findById(id,function(err,movie){
-      res.render('detail',{
-		title:'imooc' + movie.title,
-		movie:movie
-	})
-  })
-})
-
-
-
-//admin page 
-
-
-app.get('/admin/movie',function(req,res){
-	res.render('admin',{
-		title:'imooc 后台录入页',
-		movie:{
-			title: '',
-			doctor: '',
-			country: '',
-			year: '',
-			poster: '',
-			flash: '',
-			summary: '',
-			language: ''
-		}
-	})
-})
-
-
-//admin update movie 
-app.get('/admin/update/:id',function(req,res){
-	var id = req.params.id
-
-	if(id) {
-		Movie.findById(id,function(err,movie){
-			res.render('admin',{
-				title: 'imooc 后台更新页',
-				movie: movie
-			})
-		})
-	}
-})
-
-
-// admin post movie
-app.post('/admin/movie/new',function(req,res){
-	var id = req.body.movie._id
-	var movieObj = req.body.movie
-	var _movie
-
-	if(id !== 'undefined'){
-		Movie.findById(id ,function(err,movie){
-			if(err){
-				console.log(err)
-			}
-
-			_movie = _.extend(movie,movieObj)
-			_movie.save(function(err,movie){
-				if(err){
-					console.log(err)
-				}
-
-				res.redirect('/movie/' +movie._id)
-			})
-		})
-	}
-    else{
-    	_movie = new Movie({
-    		doctor: movieObj.doctor,
-    		title: movieObj.title,
-    		country: movieObj.country,
-    		language: movieObj.language,
-    		year: movieObj.year,
-    		poster: movieObj.poster,
-    		summary: movieObj.summary,
-    		flash: movieObj.flash
-    	})
-
-    	_movie.save(function(err,movie){
-    		if(err){
-					console.log(err)
-				}
-
-				res.redirect('/movie/' +movie._id)
-    	})
-    }
-})
-//list page 
-
-
-app.get('/admin/list',function(req,res){
-	Movie.fetch(function(err,movies){
-      if(err){
-      	console.log(err)
-      }
-
-     
-     res.render('list',{
-		title:'imooc 列表页',
-		movies:movies
-	})
-  })
-})
-
-
-//list delete movie
-
-app.delete('/admin/list',function(req,res){
-
-    var id = req.query.id
-
-    if(id){
-        Movie.remove({_id:id},function(err,movie){
-            if(err){
-                console.log(err)
-            }
-            else{
-                res.json({success:1})
-            }
-        })
-    }
-})
+require('./config/routes')(app)
---
+++ imooc-projects/imooc/app/controllers/comment.js
@@ -0,0 +1,21 @@
+//var _ = require('underscore')
+var Comment = require('../models/comment')
+
+
+
+
+// comment
+exports.save = function(req,res){
+    var _comment = req.body.comment
+    var movieId = _comment.movie
+    var comment = new Comment(_comment)
+         //console.log(from)
+        comment.save(function(err,comment){
+            if(err){
+                    console.log(err)
+                }
+
+                res.redirect('/movie/' + movieId)
+        })
+}
+
