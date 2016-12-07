258bbe18334ea8106da20372c012c56a2cbdef59
qianlaibao22 <511718974@qq.com>
2016-12-07 13:12:41 +0800

    添加了分页功能，搜索功能

--- imooc-projects/imooc/app/controllers/index.js
+++ imooc-projects/imooc/app/controllers/index.js
@@ -1,25 +1,90 @@
 var Movie = require('../models/movie')
 var Category = require('../models/category')
+
+
+
+//index page
 exports.index = function(req,res){
-    console.log('user in session:')
-    console.log(req.session.user)
+    /*console.log('user in session:')
+    console.log(req.session.user)*/
 
     Category
        .find({})
-       .populate({path: 'movies',options: {limit: 5}})
+       .populate({
+        path: 'movies',
+        options: {limit: 5},
+        select:'title poster'
+      })
        .exec(function(err,categories){
 
       if(err){
         console.log(err)
       }
 
-     
+     console.log('category:')
+      console.log(categories)
      res.render('index',{
         title:'imooc 首页',
         categories:categories
     }) 
-  })
-      
+  })  
+}
 
+//search page
+exports.search = function(req,res){
+  var catId = req.query.cat
+  var q = req.query.q
+  var page = parseInt(req.query.p, 10) || 0
+  var count= 2
+  var index = page*count
+
+  if(catId){
+    Category
+       .find({_id: catId})
+       .populate({
+        path: 'movies',
+        select:'title poster'
+     })
+       .exec(function(err,categories){
+
+      if(err){
+        console.log(err)
+      }
+
+     var category = categories[0] || {}
+     var movies = category.movies || []
+     var results = movies.slice(index,index + count)
+     
+     res.render('results',{
+        title:'imooc 结果列表页面',
+        keyword: category.name,
+        currentPage: (page + 1),
+        query: 'cat=' + catId,
+        totalPage: Math.ceil(movies.length / count),
+        movies: results
+    }) 
+  })  
+  }
+    else{
+      Movie
+        .find({title: new RegExp(q + '.*', 'i')})
+        .exec(function(err,movies){
+          if(err){
+        console.log(err)
+      }
+
+     var results = movies.slice(index,index+count)
+
+     res.render('results',{
+        title:'imooc 结果列表页面',
+        keyword: q,
+        currentPage: (page + 1),
+        query: 'q=' + q,
+        totalPage: Math.ceil(movies.length / count),
+        movies: results
+    }) 
+  })  
+
+    }
    
 }
